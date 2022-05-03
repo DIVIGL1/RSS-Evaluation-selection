327d74337c51c443b853479b3999e5b3ff0b3e47
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2022-05-03 22:19:15 +0300

    The functionality has been redesigned using the KFold and cross_val_score modules.

--- src/rss9module/train.py
+++ src/rss9module/train.py
@@ -4,11 +4,12 @@ import click
 import mlflow
 import mlflow.sklearn
 from joblib import dump
-from sklearn.metrics import (accuracy_score, balanced_accuracy_score,
-                             hamming_loss, matthews_corrcoef, precision_score)
+from numpy import mean
+from sklearn.model_selection import KFold, cross_val_score
 
 from .data import get_datasets
 from .pipeline import create_pipeline
+
 
 @click.command()
 @click.option(
@@ -59,69 +60,64 @@ def compute_model(
     dataset_path: Path = Path("data/train.csv"),
     save_model_path: Path = Path("data/unknown_model.joblib"),
     random_state: int = 42,
-    test_size: float = 0.2,
+    test_size: float = 0.0,
     use_scaler: bool = True,
     n_estimators: int = 100,
     criterion: str = "gini",
     max_depth: int = None,
     max_features: str = "auto"
 ):
-    # Получим наборы данных для обучения и валидации:
-    features_train, features_val, target_train, target_val = get_datasets(
-        dataset_path,
-        random_state,
-        test_size,
-    )
-    with mlflow.start_run():
-        # Соберём параметры для передачи в функцию:
-        params = {
-            "random_state": random_state,
-            "use_scaler": use_scaler,
-            "n_estimators": n_estimators,
-            "criterion": criterion,
-            "max_depth": max_depth,
-            "max_features": max_features,
-        }
-        # Запустим процедуры в соответствии с pipeline:
-        pipeline = create_pipeline(**params)
-        pipeline.fit(features_train, target_train)
+    # Получим набор данных
+    # (если передадит test_size=0.0, то разбиения не будет):
+    x_train, _, y_train, _ = get_datasets(dataset_path)
+#     with mlflow.start_run():
+    # Соберём параметры для передачи в функцию:
+    params = {
+        "random_state": random_state,
+        "use_scaler": use_scaler,
+        "n_estimators": n_estimators,
+        "criterion": criterion,
+        "max_depth": max_depth,
+        "max_features": max_features,
+    }
+    # Запишем данные в MLFlow:
+    mlflow.log_param("random_state", random_state)
+    mlflow.log_param("use_scaler", use_scaler)
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("criterion", criterion)
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_param("max_features", max_features)
 
-        # на основании обсчитанно модели получим
-        # предсказание для валидационной выборки:
-        predict_val = pipeline.predict(features_val)
+    # Запустим процедуры в соответствии с pipeline:
+    model = create_pipeline(**params)
+    model.fit(x_train, y_train)
 
-        # вычислим три разные метрики по результатам работы модели:
-        acc = round(accuracy_score(target_val, predict_val), 4)
-        balanced_acc = round(
-            balanced_accuracy_score(target_val, predict_val), 4
+    # Список названий оценок:
+    scores_list = [
+        "r2",
+        "accuracy",
+        "homogeneity_score",
+        "neg_mean_absolute_error",
+        "f1_macro",
+        "roc_auc_ovr",
+    ]
+
+    # вычислим разные метрики по результатам cross_val_score
+    # с использованием подготовленной модели model:
+    cv = KFold(n_splits=10, random_state=random_state, shuffle=True)
+
+    print("---------------------------------------------")
+    print("Значение расчитанных метрик:")
+    for one_score in scores_list:
+        scores = cross_val_score(
+            model, x_train, y_train, scoring=one_score, cv=cv, n_jobs=-1
         )
-        m_corrcoef = round(matthews_corrcoef(target_val, predict_val), 4)
-        precision_score_w = round(
-            precision_score(target_val, predict_val, average="weighted"), 4
-        )
-        h_loss = round(hamming_loss(target_val, predict_val), 4)
+        print_and_save_result(one_score, round(mean(scores), 4))
+    print("---------------------------------------------")
 
-        # Запишем данные в MLFlow:
-        mlflow.log_param("random_state", random_state)
-        mlflow.log_param("use_scaler", use_scaler)
-        mlflow.log_param("n_estimators", n_estimators)
-        mlflow.log_param("criterion", criterion)
-        mlflow.log_param("max_depth", max_depth)
-        mlflow.log_param("max_features", max_features)
-
-        # Выведем значения метрик:
-        print("---------------------------------------------")
-        print("Значение расчитанных метрик:")
-        print_and_save_result("accuracy", acc)
-        print_and_save_result("balanced accuracy", balanced_acc)
-        print_and_save_result("Matthews correlation coefficient", m_corrcoef)
-        print_and_save_result("precision score weighted", precision_score_w)
-        print_and_save_result("Hamming loss", h_loss)
-        print("---------------------------------------------")
-
-        # Сохраним модель по переданному пути:
-        dump(pipeline, save_model_path)
-        print(f"Model is saved to {save_model_path}")
+    # Сохраним модель по переданному пути:
+    dump(model, save_model_path)
+    print(f"Model is saved to {save_model_path}")
 
 def print_and_save_result(title: str, value: float) -> None:
     print(f"    {title}: {value}")
