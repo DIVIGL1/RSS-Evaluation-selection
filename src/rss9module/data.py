7da4b48f886edecd3b1f86e2db6c9a45277e4f8f
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2022-05-03 19:12:59 +0300

    Function of data preparation was updated.

--- src/rss9module/data.py
+++ src/rss9module/data.py
@@ -2,31 +2,29 @@ from pathlib import Path
 from typing import Tuple
 import pandas as pd
 from sklearn.model_selection import train_test_split
-import click
 
 
 def get_datasets(
-    csv_path: Path, random_state: int, test_split_ratio: float
+    csv_path: Path = Path("data/train.csv"),
+    random_state: int = 42,
+    test_size: float = 0.2
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
-    
+
     dataset = pd.read_csv(csv_path)
-    click.echo(f"Common Dataset shape is: {dataset.shape}.")
+    dataset.drop("Id", axis=1, inplace=True)
+    print(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop("Cover_Type", axis=1)
     target = dataset["Cover_Type"]
-    
-    features_train, features_val, target_train, target_val = \
+
+    return(
         train_test_split(
-            features, 
-            target, 
-            test_size=test_split_ratio, 
+            features,
+            target,
+            test_size=test_size,
             random_state=random_state
         )
-
-    click.echo(f"Train Dataset shape is: {features_train.shape}.")
-    click.echo(f"Validate Dataset shape is: {features_val.shape}.")
-    
-    return features_train, features_val, target_train, target_val
+    )
 
 if __name__ == '__main__':
     ppath = Path("data/train.csv")
-    data = get_datasets(ppath, 42, 0.3)
+    data = get_datasets(ppath, 42, 0.2)
