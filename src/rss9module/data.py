6c895aade18ce17e6a6093fc5bf444e10eda4731
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2022-05-03 22:18:11 +0300

    Added the ability to return simple data without splitting into test and validation data.

--- src/rss9module/data.py
+++ src/rss9module/data.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 def get_datasets(
     csv_path: Path = Path("data/train.csv"),
     random_state: int = 42,
-    test_size: float = 0.2
+    test_size: float = 0.0
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
 
     dataset = pd.read_csv(csv_path)
@@ -16,15 +16,17 @@ def get_datasets(
     features = dataset.drop("Cover_Type", axis=1)
     target = dataset["Cover_Type"]
 
-    return(
-        train_test_split(
-            features,
-            target,
-            test_size=test_size,
-            random_state=random_state
+    if test_size == 0.0:
+        return (features, None, target, None)
+    else:
+        return(
+            train_test_split(
+                features,
+                target,
+                test_size=test_size,
+                random_state=random_state
+            )
         )
-    )
 
 if __name__ == '__main__':
-    ppath = Path("data/train.csv")
-    data = get_datasets(ppath, 42, 0.2)
+    data = get_datasets(Path("data/train.csv"), 42, 0.2)
