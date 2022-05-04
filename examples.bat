23f7d47cbecbfcfd0f3e015ad043c304c0932711
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2022-05-04 22:39:14 +0300

    Windows bat-file which provides 9 examples with three examples for each model. Variety of data preparation is provided by the "--fe-type" parameter, which takes the value 0, 1, 2.

---
+++ examples.bat
@@ -0,0 +1,11 @@
+call poetry run train --fe-type 0 --model "rfc"
+call poetry run train --fe-type 1 --model "rfc" --n-estimators 200 --criterion "entropy" --max-depth 10 --max-features "log2"
+call poetry run train --fe-type 2 --model "rfc" --n-estimators 50 --max-depth 10 --max-features "sqrt"
+
+call poetry run train --fe-type 0 --model "knn" 
+call poetry run train --fe-type 1 --model "knn" --n-neighbors 3 --weights "distance" --algorithm "ball_tree"
+call poetry run train --fe-type 2 --model "knn" --n-neighbors 7 --weights "uniform"
+
+call poetry run train --fe-type 0 --model "svc" 
+call poetry run train --fe-type 1 --model "svc" --c-param 10 --kernel "sigmoid"  --tol 0.0001
+call poetry run train --fe-type 2 --model "svc" --c-param 20 --kernel "poly" --shrinking False --tol 0.01
