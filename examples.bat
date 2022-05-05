a654521d78bd52c4d59c71efcf6c3196da0d9ecf
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2022-05-05 17:09:25 +0300

    The list of experiments has been updated.

--- examples.bat
+++ examples.bat
@@ -1,11 +1,27 @@
 call poetry run train --fe-type 0 --model "rfc"
-call poetry run train --fe-type 1 --model "rfc" --n-estimators 200 --criterion "entropy" --max-depth 10 --max-features "log2"
-call poetry run train --fe-type 2 --model "rfc" --n-estimators 50 --max-depth 10 --max-features "sqrt"
+call poetry run train --fe-type 1 --model "rfc"
+call poetry run train --fe-type 2 --model "rfc"
+call poetry run train --fe-type 3 --model "rfc"
 
-call poetry run train --fe-type 0 --model "knn" 
-call poetry run train --fe-type 1 --model "knn" --n-neighbors 3 --weights "distance" --algorithm "ball_tree"
-call poetry run train --fe-type 2 --model "knn" --n-neighbors 7 --weights "uniform"
+call poetry run train --fe-type 0 --model "knn"
+call poetry run train --fe-type 1 --model "knn"
+call poetry run train --fe-type 2 --model "knn"
+call poetry run train --fe-type 3 --model "knn"
 
-call poetry run train --fe-type 0 --model "svc" 
-call poetry run train --fe-type 1 --model "svc" --c-param 10 --kernel "rbf"  --tol 0.0001
-call poetry run train --fe-type 2 --model "svc" --c-param 20 --kernel "poly" --shrinking False --tol 0.01
+call poetry run train --fe-type 0 --model "svc"
+call poetry run train --fe-type 1 --model "svc"
+call poetry run train --fe-type 2 --model "svc"
+call poetry run train --fe-type 3 --model "svc"
+
+
+call poetry run train --fe-type 3 --model "rfc" --n-estimators 100 --max-depth 10 --max-features "log2"
+call poetry run train --fe-type 3 --model "rfc" --n-estimators 200 --criterion "entropy" --max-features "sqrt"
+call poetry run train --fe-type 3 --model "rfc" --n-estimators 500 --random-state 10 --max-features "auto"
+
+call poetry run train --fe-type 3 --model "knn" --n-neighbors 3 --weights "distance" --algorithm "ball_tree"
+call poetry run train --fe-type 3 --model "knn" --n-neighbors 5 --weights "uniform" --algorithm "auto"
+call poetry run train --fe-type 3 --model "knn" --n-neighbors 10
+
+call poetry run train --fe-type 3 --model "svc" --c-param 0.1
+call poetry run train --fe-type 3 --model "svc" --c-param 10 --kernel "rbf"  --tol 0.0001
+call poetry run train --fe-type 3 --model "svc" --c-param 20 --kernel "poly" --shrinking False --tol 0.01
