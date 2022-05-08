0851c917e094a3efbbaeb4ea53e6c8648a82d1ba
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2022-05-08 19:59:18 +0300

    Updated batch file with examples for running task No.7. Added batch file to run examples for task No.9.

---
+++ examples-nested.bat
@@ -0,0 +1,3 @@
+call poetry run train --fe-type 3 --model-type "rfc" --nested-cv True
+call poetry run train --fe-type 3 --model-type "knn" --nested-cv True
+call poetry run train --fe-type 3 --model-type "svc" --nested-cv True
--- examples.bat
+++ examples.bat
@@ -14,14 +14,14 @@ call poetry run train --fe-type 2 --model-type "svc"
 call poetry run train --fe-type 3 --model-type "svc"
 
 
+call poetry run train --fe-type 3 --model-type "rfc" --n-estimators 10 --criterion "entropy" --max-features "sqrt"
 call poetry run train --fe-type 3 --model-type "rfc" --n-estimators 100 --max-depth 10 --max-features "log2"
-call poetry run train --fe-type 3 --model-type "rfc" --n-estimators 200 --criterion "entropy" --max-features "sqrt"
-call poetry run train --fe-type 3 --model-type "rfc" --n-estimators 500 --random-state 10 --max-features "auto"
+call poetry run train --fe-type 3 --model-type "rfc" --n-estimators 200 --criterion "gini" --random-state 10 --max-features "auto"
 
 call poetry run train --fe-type 3 --model-type "knn" --n-neighbors 3 --weights "distance" --algorithm "ball_tree"
-call poetry run train --fe-type 3 --model-type "knn" --n-neighbors 5 --weights "uniform" --algorithm "auto"
-call poetry run train --fe-type 3 --model-type "knn" --n-neighbors 10
+call poetry run train --fe-type 3 --model-type "knn" --n-neighbors 6 --weights "uniform" --algorithm "auto"
+call poetry run train --fe-type 3 --model-type "knn" --n-neighbors 8
 
 call poetry run train --fe-type 3 --model-type "svc" --c-param 0.1
-call poetry run train --fe-type 3 --model-type "svc" --c-param 10 --kernel "rbf"  --tol 0.0001
+call poetry run train --fe-type 3 --model-type "svc" --c-param 10 --kernel "rbf"  --shrinking True --tol 0.0001
 call poetry run train --fe-type 3 --model-type "svc" --c-param 20 --kernel "poly" --shrinking False --tol 0.01
