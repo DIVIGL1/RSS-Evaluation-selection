8661d56a3ca6640d44a0d8188bc1d6dc2606aa61
Dmitry Gluzdov <Dmitry@Gluzdov.ru>
2022-05-10 12:36:31 +0300

    All utils were installed and configured (nox, flake8, mypy)

---
+++ noxfile.py
@@ -0,0 +1,30 @@
+from typing import Any
+import nox
+
+
+@nox.session
+def flake8(session: Any) -> (None):
+    session.run("flake8", "src/rss9module/train.py", external=True)
+    session.run("flake8", "src/rss9module/data.py", external=True)
+    session.run("flake8", "src/rss9module/pipeline.py", external=True)
+    session.run("flake8", "src/rss9module/__init__.py", external=True)
+    session.run("flake8", "test/test_all.py", external=True)
+    session.run("flake8", "setup.py", external=True)
+
+
+@nox.session
+def black(session: Any) -> (None):
+    session.run("black", ".", external=True)
+
+
+@nox.session
+def mypy(session: Any) -> (None):
+    session.run("mypy", ".", external=True)
+    # session.run("mypy", "src/rss9module/train.py", external=True)
+    # session.run("mypy", "src/rss9module/data.py", external=True)
+    # session.run("mypy", "src/rss9module/pipeline.py", external=True)
+
+
+@nox.session
+def tests(session: Any) -> (None):
+    pass
